12fdd056cd397658bdcf32bd347137ef531a87da
RyanCheungJF <ryan9cheung@gmail.com>
2022-07-09 00:01:47 +0800

    Style main cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,19 +7,36 @@ import { CardProps } from "../types/cardprops";
 
 const Card: React.FC<CardProps> = ({ item }) => {
   return (
-    <div className="bg-blue-500 rounded-lg object-contain w-50 h-50">
-      <Link to={"/items/" + item.frontmatter.slug} key={item.id}>
-        <GatsbyImage
-          image={getImage(
-            item.frontmatter.thumb.childImageSharp.gatsbyImageData
-          )}
-          alt="banner"
-        />
-        <div>
-          <h1> {item.frontmatter.title}</h1>
-          <h1> {item.frontmatter.price}</h1>
+    <div className="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-md hover:shadow-lg">
+      <div className="p-2">
+        <h2 className="font-bold text-lg mb-2">{item.frontmatter.title}</h2>
+        <Link to={"/items/" + item.frontmatter.slug} key={item.id}>
+          <GatsbyImage
+            className="h-40 rounded-xl object-cover object-center"
+            image={getImage(
+              item.frontmatter.thumb.childImageSharp.gatsbyImageData
+            )}
+            alt="banner"
+          />
+        </Link>
+        <div className="py-1" />
+        <h1 className="text-black font-semibold">{item.frontmatter.price}</h1>
+        <div className="py-1" />
+        <div className="h-40">
+          <p className="text-sm text-gray-600 text-ellipsis">
+            {item.frontmatter.description}
+          </p>
         </div>
-      </Link>
+      </div>
+      <div className="m-2">
+        <a
+          role="button"
+          href="#"
+          className="text-white font-semibold bg-yellow-500 px-3 py-1 rounded-md hover:bg-yellow-700"
+        >
+          Learn More
+        </a>
+      </div>
     </div>
   );
 };
--- src/items/chickenRice.md
+++ src/items/chickenRice.md
@@ -1,6 +1,6 @@
 ---
-title: Chicken rice
-description: Freshly cooked 
+title: Chicken Rice
+description: Freshly cooked chicken extremely nice please buy if not I'll go bankrupt please I have a family.
 price: $5000000
 slug: chicken-rice
 thumb: ../images/chicken-rice.jpg
