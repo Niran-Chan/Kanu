379ea28266e60ea38e73484705f2ac5039d918c6
S A <amruthsrivathsancode@gmail.com>
2022-07-09 20:05:59 +0800

    added upload function 4 for bois

--- src/pages/sell.tsx
+++ src/pages/sell.tsx
@@ -3,6 +3,9 @@ import PrivateRoute from "../components/PrivateRoute";
 import Layout from "../components/Layout";
 import { navigate } from "gatsby";
 
+
+const GETFORM_FORM_ENDPOINT = "YOUR-FORM-ENDPOINT";
+
 export default function Sell() {
   const [productName, setProductName] = useState("");
   const [description, setDescription] = useState("");
@@ -21,6 +24,98 @@ export default function Sell() {
     // add to states
   };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const [formStatus, setFormStatus] = useState(false);
+    const [query, setQuery] = useState({
+        name: "",
+        email: "",
+    });
+
+    const handleFileChange = () => (e) => {
+        setQuery((prevState) => ({
+            ...prevState,
+            files: e.target.files[0]
+        }));
+    };
+
+    const handleChange = () => (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setQuery((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        Object.entries(query).forEach(([key, value]) => {
+            formData.append(key, value);
+        });
+
+        axios
+            .post(
+                GETFORM_FORM_ENDPOINT,
+                formData,
+                {headers: {Accept: "application/json"}}
+            )
+            .then(function (response) {
+                setFormStatus(true);
+                setQuery({
+                    name: "",
+                    email: "",
+                });
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
   return (
     <PrivateRoute>
       <Layout
@@ -96,6 +191,81 @@ export default function Sell() {
                       type="number"
                     />
                   </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  <div class="container-md">
+            <h2>Gatsby File Upload Form using Getform.io</h2>
+            <form
+                acceptCharset="UTF-8"
+                method="POST"
+                enctype="multipart/form-data"
+                id="gatsbyForm"
+                onSubmit={handleSubmit}
+            >
+                <div className="form-group mb-2">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter your email"
+                        required
+                        name="email"
+                        value={query.email}
+                        onChange={handleChange()}
+                    />
+                </div>
+                <div className="form-group mb-2">
+                    <label for="exampleInputName">Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter your name"
+                        required
+                        name="name"
+                        value={query.name}
+                        onChange={handleChange()}
+                    />
+                </div>
+                <hr/>
+                <div className="form-group mt-3">
+                    <label className="mr-2">Upload your FUCKING GAY ASS PHOTOS:</label>
+                    <input name="file" type="file" onChange={handleFileChange()}/>
+                </div>
+                <hr/>
+                {formStatus ? (
+                    <div className="text-success mb-2">
+                        Your message has been sent.
+                    </div>
+                ) : (
+                    ""
+                )}
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
                 </div>
                 <button
                   className="w-full my-2 py-2 bg-red-500 rounded text-white hover:shadow-md hover:shadow-teal-500/20"
